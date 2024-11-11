17a362d4389bdcfc12d9ff712d4a6e59eeac7c83
MrLylas <delmas.denilson.pro@gmail.com>
2024-11-11 23:55:50 +0100

    exo 8 php

--- exo_php8.php
+++ exo_php8.php
@@ -1,2 +1,18 @@
+
 <?php
 
+
+$cadres = ["image1","image2","image3","image4"];
+$lien = "https://my.mobirise.com/data/userpic/764.jpg";
+
+function repeterImage($cadres, $lien){
+    $images = "<div>";
+
+    foreach($cadres as $value){
+        $images .= "<img src='$lien' alt='Petit chihuaha tenant une peluche'>";
+    }
+    $images .= "</div>";
+    return $images;
+}
+
+echo repeterImage($cadres, $lien);
